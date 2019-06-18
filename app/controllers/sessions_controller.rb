3aa8bc27e1f32ada7dfed1993432ad903446abb2
github username <github email address>
2019-06-18 12:02:33 -0400

    added login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    # get the user from the database
+    @customer = Customer.find_by(name: params[:name])
+    # if they're right
+    if @customer
+        # set a cookie with that user's ID
+        session["user_id"] = @customer.id
+        redirect_to bakeries_path
+    else
+    # if they're wrong
+        # re-do the page
+        flash.notice = "No customer with that username.  Please try again."
+        render :new
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to login_path
+  end
+end
