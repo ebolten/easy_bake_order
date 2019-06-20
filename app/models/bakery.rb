236cc207d65012b10403181d23f4cdbfb86b409c
Emily Bolten <emilybolten@gmail.com>
2019-06-20 10:38:19 -0400

    fixed some errors and changed colors

--- app/models/bakery.rb
+++ app/models/bakery.rb
@@ -3,11 +3,18 @@ class Bakery < ApplicationRecord
 
     def self.most_popular
         bakeries = []
+        
         Order.all.each do |order|
             menu_item = MenuItem.find(order.menu_item_id)
             bakeries << Bakery.find(menu_item.bakery_id)
         end
-        return bakeries.max_by { |i| bakeries.count(i) }.name
+        pop_bakery = bakeries.max_by { |i| bakeries.count(i) }
+        
+        if pop_bakery == nil
+            return "No Orders."
+        else
+            return pop_bakery.name
+        end
     end
 
 end
